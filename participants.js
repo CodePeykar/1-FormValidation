f920c7626c89876cc8d383d654b803e483288fa2
Code Peykar <198622616+CodePeykar@users.noreply.github.com>
2025-02-13 19:47:49 +0330

    Update participants.js

--- participants.js
+++ participants.js
@@ -1,62 +1,52 @@
 // هیچ چیزی رو از این فایل حذف نکنید صرفا یه آبجکت مانند نمونه در آرایه اضافه کنید و اطلاعات خودتون رو وارد کنید
 // name => اسم خودتون
 // username => آدرس گیتهاب‌تون
-// source => سورس پروژه که می‌تونه کدپن یا مخزن گیتهاب باشه
-// preview => خروجی آنلاین که میتونه کدپن یا گیتهاب پیج و ... باشه
+// codepen => لینک کد پن چالش
 const participants = [
   // آبجکت اول این آرایه مثال هست و حذف نکنیدش. فقط یک آبجت به آرایه اضافه کنید و اطلاعات خودتون رو وارد کنید
   {
-    name: 'Tara',
-    username: 'github.com/codePeykar',
-    source: 'example.com',
-    preview: 'example.com',
+    name: 'Code Peykar',
+    username: 'https://github.com/codePeykar',
+    codepen: 'https://codepen.io/codepeykar/pen/vEYELmb',
   },
   {
     name: 'Farzane',
     username: 'https://github.com/farzane-na',
-    source: 'https://codepen.io/Farzane-Nazmabadi/pen/XJWraMY',
-    preview: 'https://codepen.io/Farzane-Nazmabadi/pen/XJWraMY',
+    codepen: 'https://codepen.io/Farzane-Nazmabadi/pen/XJWraMY',
   },
   {
     name: 'Kimia',
     username: 'https://github.com/Kimia-msv',
-    source: 'https://codepen.io/Kimia-msv/pen/emYYdpE',
-    preview: 'https://codepen.io/Kimia-msv/pen/emYYdpE',
+    codepen: 'https://codepen.io/Kimia-msv/pen/emYYdpE',
   },
   {
     name: 'Aria Barzegar',
     username: 'https://github.com/ariabarzegar',
-    source: 'https://codepen.io/aria-barzegar/pen/azbbbBp',
-    preview: 'https://codepen.io/aria-barzegar/pen/azbbbBp',
+    codepen: 'https://codepen.io/aria-barzegar/pen/azbbbBp',
   },
   {
     name: 'Fatemeh',
     username: 'https://github.com/Fawtm',
-    source: 'https://codepen.io/kawhlpud-the-encoder/pen/KwKPYmm',
-    preview: 'https://codepen.io/kawhlpud-the-encoder/pen/KwKPYmm',
+    codepen: 'https://codepen.io/kawhlpud-the-encoder/pen/KwKPYmm',
   },
   {
     name: 'Ali Asadi',
     username: 'https://github.com/Imaliasadi',
-    source: 'https://codepen.io/Imaliasadi/pen/JojjNOo',
-    preview: 'https://validsignupform.netlify.app/',
+    codepen: 'https://codepen.io/Imaliasadi/pen/JojjNOo',
   },
   {
     name: 'Ali Ataei',
     username: 'https://github.com/Aljatael',
-    source: 'https://codepen.io/aljatael/pen/MYWgEBM',
-    preview: 'https://codepen.io/aljatael/pen/MYWgEBM',
+    codepen: 'https://codepen.io/aljatael/pen/MYWgEBM',
   },
   {
     name: 'Sepehr',
     username: 'https://github.com/Sepehr1naqavian',
-    source: 'https://codepen.io/Sepehr1naqavian/pen/ByaadZo',
-    preview: 'https://codepen.io/Sepehr1naqavian/pen/ByaadZo',
+    codepen: 'https://codepen.io/Sepehr1naqavian/pen/ByaadZo',
   },
   {
     name: 'Narges',
     username: 'https://github.com/nargesz13',
-    source: 'https://codepen.io/nargesz13/pen/azbbMaW',
-    preview: 'https://codepen.io/nargesz13/pen/azbbMaW',
+    codepen: 'https://codepen.io/nargesz13/pen/azbbMaW',
   },
 ];
